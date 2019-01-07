fa2c2693b61f79ec61a48c9e1c281fb121b49fb8
cwdrunner <nitroxbill@msn.com>
2019-01-06 19:31:41 -0700

    Update setup.sh
    part #1

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
 # Setup commands
+# prep
+# This might be needed for jabbapylib. pycurl fails. 
+sudo apt install libcurl4-openssl-dev libssl-dev
+sudo apt install cmake
+sudo apt-get install qt4-default
+pip install pyside
+pip install jabbapylib
 
 
